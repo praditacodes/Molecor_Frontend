c7d84b2719647fa4cb9466e2ed6bd9cd8a983c86
praditacodes <praditabadal5@gmail.com>
2025-06-16 13:55:47 +0545

    Add Skeleton component

---
+++ src/components/ui/skeleton.tsx
@@ -0,0 +1,10 @@
+// skeleton.tsx
+import React from 'react';
+
+const Skeleton = () => {
+  return (
+    <div className="animate-pulse bg-gray-300 rounded w-full h-4"></div>
+  );
+};
+
+export default Skeleton;
