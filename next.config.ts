d8295e68eea2b63783ea3bc9f42e6ef5dae69489
praditacodes <praditabadal5@gmail.com>
2025-06-21 19:13:00 +0545

    Configure remote image patterns for production

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,41 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ["i.ibb.co"],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
   images: {
-    domains: ["i.ibb.co"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/media/**', // For your local development backend
+      },
+      {
+        protocol: 'https',
+        hostname: 'molecor-backend-2.onrender.com',
+        port: '',
+        pathname: '/media/**', // For your live Render backend
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co', // Kept this in case you still need it
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
